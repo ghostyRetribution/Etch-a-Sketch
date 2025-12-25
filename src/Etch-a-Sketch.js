d587baba1bc6ed292f429eb99a9f093f53f919a4
ghostyRetribution <213485414+ghostyRetribution@users.noreply.github.com>
2025-12-25 15:34:09 +0600

    made the enter button work. moved the for loop ins ide of enterBtn event listener to create boxes based on user input.  the default number of boxes whenever the page first loads/refreshed is 1. this can be changed. be sure to move the for loop out of the enterBtn event listener.

--- src/Etch-a-Sketch.js
+++ src/Etch-a-Sketch.js
@@ -1,11 +1,25 @@
 const container = document.querySelector('.container');
-const divCount = 64;
+const input = document.querySelector('.input');
+const enterBtn = document.querySelector('.enterBtn');
+let divCount = 1;
+createDivs(divCount);
+enterBtn.addEventListener('click', () => {
+	container.innerHTML = ''; //the container becomes empty first, then runs the loop again to create divs.
+	if (input.value > 64 || input.value < 0) {
+		alert('Choose a number between 1 - 64');
+		input.value = 64;
+		createDivs(1);
+	}
+	else {
+		divCount = input.value;	
+		for (let i = 0; i < divCount; i++) {
+			createDivs(divCount);
+		} // copy this loop outside if you want to have more than 1 box whenever the page is first loaded.
+	}
+});
+
 const colors = ['red', 'blue', 'orange', 'brown', 'black', 'pink', 'violet', 'purple', 'yellow'];
 // add any color
-
-for (let i = 0; i < divCount; i++) {
-	createDivs(divCount);
-}
 
 function createDivs(divCount) {	
 	for (let i = 0; i < divCount; i++) {
